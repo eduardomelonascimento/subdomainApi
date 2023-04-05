e8d2f4fbba45072b788800edeb337053a3f810b6
Eduardo Melo <nascimentomeloedu@gmail.com>
2023-04-04 21:02:06 -0300

    feat(routes): add route to delete a domain

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ router.get("/domains/:id", DomainController.getOneDomain);
 router.get("/subdomains/:id", DomainController.getSubdomains);
 
 router.post("/domains", DomainController.createDomain)
+router.delete("/domains/:id", DomainController.deleteDomain)
 
 module.exports = router;
